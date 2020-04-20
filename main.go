87e35845c10113fd3ce5fbc9b909f1cda3b880df
hongy3025 <hongy3025@163.com>
2020-04-20 23:13:49 +0800

    format

--- main.go
+++ main.go
@@ -1,47 +1,47 @@
 package main
 
- import (
- 	"io"
- 	"log"
-	"net"
+import (
 	"flag"
- )
+	"io"
+	"log"
+	"net"
+)
 
 var localAddr *string = flag.String("local", ":8080", "local address")
 var remoteAddr *string = flag.String("remote", "127.0.0.1:11000", "remote address")
 
- func main() {
+func main() {
 
- 	ln, err := net.Listen("tcp", *localAddr)
- 	if err != nil {
- 		log.Fatal(err)
- 	}
+	ln, err := net.Listen("tcp", *localAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
 
- 	log.Println("Port forwarding server up and listening on ", *localAddr)
+	log.Println("Port forwarding server up and listening on ", *localAddr)
 
- 	for {
- 		conn, err := ln.Accept()
- 		if err != nil {
- 			log.Fatal(err)
- 		}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
 
- 		go handleConnection(conn)
- 	}
- }
+		go handleConnection(conn)
+	}
+}
 
- func forward(src, dest net.Conn) {
- 	defer src.Close()
- 	defer dest.Close()
- 	io.Copy(src, dest)
- }
+func forward(src, dest net.Conn) {
+	defer src.Close()
+	defer dest.Close()
+	io.Copy(src, dest)
+}
 
- func handleConnection(c net.Conn) {
- 	remote, err := net.Dial("tcp", *remoteAddr)
- 	if err != nil {
-		 log.Println(err)
-		 return
- 	}
+func handleConnection(c net.Conn) {
+	remote, err := net.Dial("tcp", *remoteAddr)
+	if err != nil {
+		log.Println(err)
+		return
+	}
 
- 	go forward(c, remote)
- 	go forward(remote, c)
- }
+	go forward(c, remote)
+	go forward(remote, c)
+}
